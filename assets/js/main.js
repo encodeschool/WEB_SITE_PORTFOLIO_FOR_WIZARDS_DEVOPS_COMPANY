46a97a9c01e5bfb580e73ce4145d19318c3b3a12
Ergashev Nurmukhammad <ergashev.nurik01@mail.ru>
2025-03-23 00:33:23 +0300

    some changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,3 +34,17 @@ openBtn.onclick = function() {
 closeBtn.onclick = function() {
   menu.style.top = '-100%';
 }
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  }
+});
